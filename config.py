1161755d14dbaba3179551b8625f42fa2c47b4fb
Nelson <nelsonr9608@nauta.cu>
2025-09-28 10:33:12 -0400

    eliminacion de pillow

--- config.py
+++ config.py
@@ -1,10 +1,2 @@
-import os
-
-# Configuración del Bot
-BOT_TOKEN = os.getenv("BOT_TOKEN", "tu_token_aqui")
-BASE_URL = "https://nelson_file2link.onrender.com"
-SERVER_DIR = "./server"
-
-# Tamaños máximos
-MAX_FILE_SIZE = 2000 * 1024 * 1024  # 2GB
-CHUNK_SIZE = 8192
+python-telegram-bot==20.7
+flask==2.3.3
