f93942c27a4db16c637552c85fd4a700d3150b93
Nelson <nelsonr9608@nauta.cu>
2025-11-22 23:52:22 -0500

    Subida automática

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(os.getenv("API_ID", "12345678"))
 API_HASH = os.getenv("API_HASH", "tu_api_hash")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "tu_bot_token")
 RENDER_DOMAIN = os.getenv("RENDER_DOMAIN", "https://nelson-file2link.onrender.com")
-BASE_DIR = "static"
+BASE_DIR = "storage"  # ⬅️ CAMBIADO de "static" a "storage"
 PORT = int(os.getenv("PORT", 8080))
 
 # Configuración optimizada para CPU limitada
